680426d8a75e465f84dfb7c268b4b4b47366e0b4
Tanish Bansal <tanishbansal0624@gmail.com>
2025-10-13 11:29:50 +0530

    Create q1doubly.cpp

---
+++ sheet 6/q1doubly.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+class node {
+public:
+    int data;
+    node* next;
+    node* prev;
+    node(int val) {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+class doublelink {
+    node* head;
+    node* tail;
+public:
+    doublelink() {
+        head = NULL;
+        tail = NULL;
+    }
+    void push(int val, int pos) {
+        node* newnode = new node(val);
+        if (head == NULL) {
+            head = tail = newnode;
+            head->next = head;
+            head->prev = head;
+        }
+        else {
+            if (pos == 0) {
+                newnode->next = head;
+                newnode->prev = tail;
+                head->prev = newnode;
+                tail->next = newnode;
+                head = newnode;
+            }
+            else {
+                node* temp = head;
+                for (int i = 1; i < pos; i++) {
+                    temp = temp->next;
+                }
+                newnode->next = temp->next;
+                newnode->prev = temp;
+                temp->next->prev = newnode;
+                temp->next = newnode;
+                if (temp == tail) tail = newnode;
+            }
+        }
+    }
+    void pop(int pos) {
+        if (head == NULL) return;
+        if (pos == 0) {
+            node* temp = head;
+            if (head == tail) {
+                head = tail = NULL;
+            } else {
+                head = head->next;
+                head->prev = tail;
+                tail->next = head;
+            }
+            delete temp;
+        }
+        else {
+            node* temp = head;
+            for (int i = 1; i < pos && temp->next != head; i++) {
+                temp = temp->next;
+            }
+            node* del = temp->next;
+            temp->next = del->next;
+            del->next->prev = temp;
+            if (del == tail) tail = temp;
+            delete del;
+        }
+    }
+    void display() {
+        if (head == NULL) return;
+        node* temp = head;
+        do {
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << endl;
+    }
+};
+int main() {
+    doublelink c;
+    c.push(1, 0);
+    c.push(2, 1);
+    c.push(3, 2);
+    c.push(4, 3);
+    c.display();
+    c.pop(0);
+    c.display();
+    c.pop(2);
+    c.display();
+}
