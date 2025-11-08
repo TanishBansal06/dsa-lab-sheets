6c47b2c1df408e26267878cd7d7d95762bf3cef3
Tanish Bansal <tanishbansal0624@gmail.com>
2025-11-08 11:04:19 +0530

    Create q2(b).cpp

---
+++ sheet 8/q2(b).cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    
+    node(int val){
+        data = val;
+        left=NULL;
+        right=NULL;
+    }
+};
+static int i=-1;
+node* binarytree(vector<int> &v){
+    i++;
+    if(v[i]==-1){
+        return NULL;
+    }
+    node* root = new node(v[i]);
+    root->left = binarytree(v);
+    root->right = binarytree(v);
+    return root;
+}
+int mmaxele(node* root){
+   while(root->right !=NULL){
+       root = root->right;
+   }
+    return root->data;
+}
+int main() {
+    //  vector<int> v;
+    //  for(int i=0;i<11;i++){
+    //      int n;
+    //      cin>>n;
+    //      v.push_back(n);
+    //  }
+    vector<int> v = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    // preorder(binarytree(v));
+     //cout<<endl;
+    //  inorder(binarytree(v));
+    //  cout<<endl;
+    //  post(binarytree(v));
+    node* root = binarytree(v);
+    // cout<<ssearch(root,5);
+    int max = -10;
+    cout<<mmaxele(root);
+    return 0;
+}
